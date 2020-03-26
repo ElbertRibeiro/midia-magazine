0bec1807a5e5717e942e16814fd17294b07e4452
Elbert Nilton Batista Ribeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2020-03-25 22:41:46 -0300

    Updates

---
+++ precache-manifest.2e84c3ebb386272829da2ae7bedf70ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70f0b2bacfcbe6b6ca725abd80d714d1",
+    "url": "/midia-magazine/index.html"
+  },
+  {
+    "revision": "cf27c8246ce7426f17a6",
+    "url": "/midia-magazine/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f7b4c2ab26f005a880d8",
+    "url": "/midia-magazine/static/js/2.70a8117f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/midia-magazine/static/js/2.70a8117f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf27c8246ce7426f17a6",
+    "url": "/midia-magazine/static/js/main.23e9eb85.chunk.js"
+  },
+  {
+    "revision": "9de4c1192f5d6544b239",
+    "url": "/midia-magazine/static/js/runtime-main.5e300bcc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/midia-magazine/static/media/logo.5d5d9eef.svg"
+  }
+]);
