7ce6e298f8261ee3bf2aa6865c2f9ef6a9621de7
Elbert Nilton Batista Ribeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2020-03-29 12:59:14 -0300

    up

---
+++ src/pages/Home/index.js
@@ -0,0 +1,34 @@
+//yarn run deploy
+
+import React from 'react';
+import './style.css';
+
+export default function Home() {
+    return (
+        <div className="Home">
+            <header className="Home-header">
+            </header>
+
+            <body>
+                <div>
+                    <nav>
+                    </nav>
+                </div>
+
+
+                <div>
+                    <header>
+                        <h2>Magna Etiam Lorem</h2>
+                    </header>
+                    <p>Suspendisse mauris. Fusce accumsan mollis eros.
+                    Pellentesque a diam sit amet mi ullamcorper vehicula.
+                    Integer adipiscin sem. Nullam quis massa sit amet nibh viverra malesuada.
+                    Nunc sem lacus, accumsan quis, faucibus non, congue vel, arcu, erisque hendrerit tellus.
+                    Integer sagittis. Vivamus a mauris eget arcu gravida tristique. Nunc iaculis mi in ante.</p>
+    
+                </div>
+
+            </body>
+        </div>
+    );
+}
