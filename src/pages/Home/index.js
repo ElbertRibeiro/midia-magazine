8be06db9b873d641e0e21b97d7e4308ffdf9f1d8
Elbert Nilton Batista Ribeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2020-03-29 16:13:32 -0300

    up

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,30 +5,8 @@ import './style.css';
 
 export default function Home() {
     return (
-        <div className="Home">
-            <header className="Home-header">
-            </header>
-
-            <body>
-                <div>
-                    <nav>
-                    </nav>
-                </div>
 
 
-                <div>
-                    <header>
-                        <h2>Magna Etiam Lorem</h2>
-                    </header>
-                    <p>Suspendisse mauris. Fusce accumsan mollis eros.
-                    Pellentesque a diam sit amet mi ullamcorper vehicula.
-                    Integer adipiscin sem. Nullam quis massa sit amet nibh viverra malesuada.
-                    Nunc sem lacus, accumsan quis, faucibus non, congue vel, arcu, erisque hendrerit tellus.
-                    Integer sagittis. Vivamus a mauris eget arcu gravida tristique. Nunc iaculis mi in ante.</p>
-    
-                </div>
-
-            </body>
-        </div>
+<h1>teste</h1>
     );
 }
