06def88d554fc3caec68ccf2053a83967ceb7483
Elbert Nilton Batista Ribeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2020-04-10 20:41:23 -0300

    Updates

---
+++ precache-manifest.a1d78ab03fe64e427aa75232a1f7028b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccc16a83314f5e1004c79957f1337bdc",
+    "url": "/midia-magazine/index.html"
+  },
+  {
+    "revision": "7d92acb780fb60ae303c",
+    "url": "/midia-magazine/static/css/main.dc4bdeaf.chunk.css"
+  },
+  {
+    "revision": "cbbdcd54ed7b74b9b97a",
+    "url": "/midia-magazine/static/js/2.7bea712c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/midia-magazine/static/js/2.7bea712c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d92acb780fb60ae303c",
+    "url": "/midia-magazine/static/js/main.1571134c.chunk.js"
+  },
+  {
+    "revision": "720ee5f4a13ed3f19172",
+    "url": "/midia-magazine/static/js/runtime-main.a481e331.js"
+  },
+  {
+    "revision": "cacd97fa0fa6f368d790857f2d191f04",
+    "url": "/midia-magazine/static/media/image-post.cacd97fa.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ccba101ff45813f8fdd8681e3e17b902.js"
+  "/midia-magazine/precache-manifest.a1d78ab03fe64e427aa75232a1f7028b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/midia-magazine/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
